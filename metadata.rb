515b43f746643dbca909ffd02dfaf0c7fb2431d7
Tim Smith <tsmith@chef.io>
2017-03-21 15:49:34 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'chef-apt-docker'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Sets up the Docker apt repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.1'

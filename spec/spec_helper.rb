1d0bd8feffc19733e1e00fd68c5237bc076c0ab1
Chase Bolt <chase.bolt@gmail.com>
2017-07-28 14:04:15 -0700

    updating to the latest docker repo
    Signed-off-by: Chase Bolt <chase.bolt@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,9 @@ RSpec.configure do |config|
   config.color = true               # Use color in STDOUT
   config.formatter = :documentation # Use the specified formatter
   config.log_level = :error         # Avoid deprecation notice SPAM
+
+  # run all specs when using a filter, but no spec match
+  config.run_all_when_everything_filtered = true
+
+  Ohai::Config[:log_level] = :error
 end
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,8 +1,15 @@
-describe apt('https://apt.dockerproject.org/repo') do
-  it { should exist }
-  it { should be_enabled }
+if os[:name] == 'debian'
+  describe apt('https://download.docker.com/linux/debian') do
+    it { should exist }
+    it { should be_enabled }
+  end
+elsif os[:name] == 'ubuntu'
+  describe apt('https://download.docker.com/linux/ubuntu') do
+    it { should exist }
+    it { should be_enabled }
+  end
 end
 
-describe package('docker-engine') do
+describe package('docker-ce') do
   it { should be_installed }
 end

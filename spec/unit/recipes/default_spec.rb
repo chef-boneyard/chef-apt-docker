6f5dae642cc9b91e4dd4f4c6b79b2af5c719613e
Tim Smith <tsmith@chef.io>
2017-08-07 18:10:22 -0700

    Add a test for the debian workaround
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'chef-apt-docker::default' do
-  context 'default attributes' do
+  context 'Ubuntu 16.04' do
     let(:chef_run) do
       ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe)
     end
@@ -16,6 +16,34 @@ describe 'chef-apt-docker::default' do
 
     it 'removes the apt repo docker-test' do
       expect(chef_run).to remove_apt_repository('docker-test')
+    end
+
+    it 'removes the apt repo docker-test' do
+      expect(chef_run).to remove_apt_repository('docker-test')
+    end
+
+    it 'does not install repo dependency packages' do
+      expect(chef_run).not_to install_package(%w(dirmngr apt-transport-https))
+    end
+  end
+
+  context 'Debian 8' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(platform: 'debian', version: '8.8').converge(described_recipe)
+    end
+
+    it 'does not install repo dependency packages' do
+      expect(chef_run).not_to install_package(%w(dirmngr apt-transport-https))
+    end
+  end
+
+  context 'Debian 9' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new(platform: 'debian', version: '9.0').converge(described_recipe)
+    end
+
+    it 'installs dependency repo packages' do
+      expect(chef_run).to install_package(%w(dirmngr apt-transport-https))
     end
   end
 end
